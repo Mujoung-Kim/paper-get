aa0cf26760b8f1202af2af5c2d5baabca627a70c
kwonmory <gurwns9632@naver.com>
2017-07-10 19:20:42 +0900

    app.blade.php -> 약간 수정 composer -> laracasts/flash 설치

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,24 @@
 <html lang="ko">
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{csrf_token()}}">
     <title>Paperget::Ad</title>
     @yield('style')
 </head>
-<body>
+<body id="app-layout">
+
+@include('layouts.partial.header')
+
+<div class="container">
+    @yield('content')
+</div>
+
+@include('layouts.partial.footer')
+
+<script src="js/app.js">
+
+</script>
+@yield('script')
 
 </body>
 </html>
