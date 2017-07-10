c9fcbf57c27311d18fa85538205b948e8bac4431
kwonmory <gurwns9632@naver.com>
2017-07-10 19:13:04 +0900

    app.blade.php -> 프레임 footer.blade.php -> 프레임 header.blade.php -> 프레임 web.php -> 태스트용 ==>>추가
    프레임 : 뼈대만 추가

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>Paperget::Ad</title>
+    @yield('style')
+</head>
+<body>
+
+</body>
+</html>
---
+++ resources/views/layouts/partial/header.blade.php
@@ -0,0 +1,5 @@
+<hr>
+
+Header Section
+
+<hr>
