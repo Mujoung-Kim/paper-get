8b2be8cb85a9798d23d72c3568ae2333a16d942d
kwonmory <gurwns9632@naver.com>
2017-07-10 19:48:06 +0900

    라우터 정의 -사용자 가입 -사용자 인증 -비밀번호 초기화
    라우터 정의함

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
+
+/*
+기본 베이직
+*/
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,4 +25,58 @@ Route::get('/home', function (){
 
 
 Route::get('/intro', 'HomeController@index');
+
+/*
+ * 사용자 가입
+ */
+Route::get('auth/register',[
+    'as' => 'users.create',
+    'uses' => 'UsersController@create'
+]);
+
+Route::post('auth/register',[
+    'as' => 'users.store',
+    'uses' => 'UsersController@store'
+]);
+
+Route::get('auth/confirm/{code}',[
+    'as' => 'users.confirm',
+    'uses' => 'UsersController@confirm'
+]);
+
+/*
+ * 사용자 인증
+ */
+
+Route::get('auth/login',[
+    'as' => 'sessions.create',
+    'uses' => 'SessionsController@create'
+]);
+
+Route::post('auth/logout',[
+    'as' => 'sessions.store',
+    'uses' => 'SessionsController@store'
+]);
+
+Route::get('auth/logout',[
+    'as' => 'sessions.destroy',
+    'uses' => 'SessionController@destroy'
+]);
+
+/*
+ * 비밀번호 초기화
+ */
+
+Route::get('auth/remind',[
+    'as' => 'reset.create',
+    'uses' => 'PasswordsController@getReset',
+]);
+
+Route::post('auth/reset',[
+    'as' => 'reset.store',
+    'uses' => 'PasswordsController@postRest',
+]);
+
+
+
 
